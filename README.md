5a4024c3b679ad2ed667f853980d4d0913d49a3e
Anna Tr <anna.trandafil14@gmail.com>
2024-10-26 18:39:27 +0200

    hw -3.2

---
+++ README.md
@@ -0,0 +1,7 @@
+- to work with the projectclone itfrom the repository
+
+- to install all dependencies run 'npm install'
+
+- to run tests 'npx jest'
+
+- to run tests with test coverage info 'npx jest --'
--- test_3_1.test.js
+++ test_3_1.test.js
@@ -1,1 +1,21 @@
-const calculateBonus = require('./test_3_1.js')
+ const calculateBonus = require('./test_3_1.js')
+
+  test('>50', () => {
+    expect(calculateBonus(1,50)).toEqual(50);
+});
+  test('<50', () => {
+    expect(calculateBonus(1,48)).toEqual(49);
+ });
+// test('=50', () => {
+//     expect(calculateBonus(25,25)).toEqual(50);
+// });
+//
+// test('0.01', () => {
+//     expect(calculateBonus(0,0.01)).toEqual(0.01);
+// });
+// test('-1', () => {
+//     expect(calculateBonus(-1)).toEqual(-1);
+// });
+// test('6786896896895785', () => {
+//     expect(calculateBonus(6786896896895785)).toEqual(6786896896895785);
+// });
